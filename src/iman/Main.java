280c033ddbbb59186ed17f75f74b4e5e322b2f85
iman <iman@dyneemadev.com>
2022-08-28 10:19:12 +0430

    tic tac toe board

---
+++ src/iman/Main.java
@@ -0,0 +1,21 @@
+package iman;
+
+
+public class Main{
+
+   public static void main(String[] args) {
+
+      char[] [] gameBoard = {{' ', '|', ' ', '|', ' '},
+         {'-', '+', '-', '+', '-'},
+         {' ', '|', ' ', '|', ' '},
+         {'-', '+', '-', '+', '-'},
+         {' ', '|', ' ', '|', ' '},
+      };
+      for (char[] row : gameBoard) {
+         for (char c : row) {
+            System.out.print(c);
+         }
+         System.out.println();
+      }
+   }
+}
